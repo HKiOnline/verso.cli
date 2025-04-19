51f3a38fe247f314120509192baf5f61999dbfbc
Jose Kortelahti <jkortelahti@gmail.com>
2025-04-19 22:32:46 +0300

    Implement app initialization

--- internal/app/app.go
+++ internal/app/app.go
@@ -1,1 +1,36 @@
 package app
+
+import (
+	"flag"
+)
+
+const defaultArg string = "latest"
+
+type app struct {
+	arg string
+}
+
+// New initializes and returns the app struct. Possible command-line arguments and flags
+// are parsed, and if not present, default values are used.
+func New() (*app, error) {
+
+	app := &app{
+		arg: getFlagArg(),
+	}
+
+	// TODO: Handle errors - if logic doesn't need to handle errors, remove the error return value!
+
+	return app, nil
+}
+
+func getFlagArg() string {
+	flag.Parse()
+
+	args := flag.Args()
+
+	if len(args) == 0 {
+		return defaultArg
+	}
+
+	return args[0]
+}
