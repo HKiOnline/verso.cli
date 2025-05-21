87543a0ba73c2c202b94a0fcf0ae3f1030378fb8
Henri Kesseli <HKiOnline@users.noreply.github.com>
2025-05-21 21:14:15 +0300

    Added tests for list and latest

--- internal/output/latest.go
+++ internal/output/latest.go
@@ -7,6 +7,11 @@ import (
 
 // Output the latest version found from the changelog
 func Latest(changelog *verso.Changelog) string {
+
+	if len(changelog.Versions) < 1 {
+		return ""
+	}
+
 	version := changelog.Versions[0]
 	return fmt.Sprintf("%d.%d.%d\n", version.Major, version.Minor, version.Patch)
 }
